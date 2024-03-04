a8950bf478f62ec519d0c22e116169cc2b7007df
FrancescoBonanno1 <francescosoapbonanno@gmail.com>
2024-03-04 16:49:55 +0100

    progressi attuali

---
+++ vite-project/src/pages/App.Detail.vue
@@ -0,0 +1,43 @@
+<script>
+import {store} from '../store';
+import axios from "axios";
+
+export default{
+name: "AppDetail",
+props:["operators"],
+data (){
+return {
+    store,
+    event :null
+}
+},
+mounted(){
+ this.event = this.store.operators.find(item => item.id == this.id);
+console.log(this.event);
+this.getEventId();},
+methods : {
+    getEventId(){
+        let url = this.store.
+    }
+}
+
+
+
+
+}
+
+
+
+</script>
+
+<template>
+
+
+</template>
+
+
+<style scoped>
+
+
+
+</style>
