cf660ea6ee9cdbd71ec681dd9494764470f9f871
FrancescoBonanno1 <francescosoapbonanno@gmail.com>
2024-03-05 02:09:04 +0100

    abbozzo ricerca collegata con id di operatori e specializzazioni

--- vite-project/src/pages/App.Detail.vue
+++ vite-project/src/pages/App.Detail.vue
@@ -1,14 +1,13 @@
 <script>
 import axios from "axios";
-import { store } from "../store.js"; // Importa il file store.js
-
+import { store } from "../store.js";
 export default {
   name: "Detail",
   props: ["id"],
   data() {
     return {
       detail: null,
-      store // Aggiungi store come dato del componente
+      store 
     };
   },
   mounted() {
@@ -16,12 +15,22 @@ export default {
   },
   methods: {
     getDetail() {
-      let url = this.store.apiUrl + "/" + this.operator_specializations.operator_id; // Utilizza l'URL dallo store
+      let url = this.store.apiUrl + "/" + this.operator_specializations.operator_id; 
 
       axios.get(url)
         .then(response => {
           if (response.status === 200 && response.data.success) {
-            this.event = response.data.payload;
+            this.detail = response.data.payload;
+
+           
+          const operatorID = this.detail.operator_id;
+          this.detail.operator = this.store.operators.find(operator => operator.id === operatorID);
+
+         
+          const specializationID = this.detail.specialization_id;
+
+
+          this.detail.specialization = this.store.specializations.find(specialization => specialization.id === specializationID);
           } else {
             console.error("Errore nella richiesta dei dettagli dell'utente.");
           }
@@ -37,7 +46,21 @@ export default {
 <template>
 <div v-for="dato in store.operator_specializations">
 <h1>{{ dato.operator_id }}</h1>
+<p>{{ dato.specialization_id }}</p>
 </div>
+
+<div v-if="detail.operator">
+  <h2>Operatore:</h2>
+  <p>Nome: {{ detail.operator.name }}</p>
+  <p>Email: {{ event.operator.email }}</p>
+</div>
+
+<div v-if="detail.specialization">
+  <h2>Specializzazione:</h2>
+  <p>Nome: {{ detail.specialization.name }}</p>
+</div>
+
+
 
 </template>
 
