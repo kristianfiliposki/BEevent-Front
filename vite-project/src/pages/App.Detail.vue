47aa877f768deb7b5dbda7832eb52f448593830f
FrancescoBonanno1 <francescosoapbonanno@gmail.com>
2024-03-06 11:55:35 +0100

    css detail

--- vite-project/src/pages/App.Detail.vue
+++ vite-project/src/pages/App.Detail.vue
@@ -17,19 +17,90 @@ export default {
   },
 };
 </script>
-<template>
-<div v-if="operator">
-    <h2>Dettagli di {{ operator.name }}</h2>
-    <h3>Descrizione: {{ operator.description }}</h3>
-    <h4>Prezzo di ingaggio : {{ operator.engagement_price }}</h4>
 
+<template>
+  <div class="container" v-if="operator">
+    <div class="card">
+      <h1>Dettagli di {{ operator.name }}</h1>
+      <h2>{{ operator.description }}</h2>
+      <p>{{ operator.address }}</p>
+      <p>In attività dal :{{ operator.foundation_year }}</p>
+      <p>recapito telefonico:{{ operator.phone }}</p>
+      <h4>Prezzo di ingaggio : {{ operator.engagement_price }}</h4>
+    </div>
+
+  </div>
+  <div class="btnCont">
+    <button class="btn">
+      <H3>CONTATTA</H3>
+    </button>
+
+    <button class="btn">
+      <H3>DAI UN VOTO</H3>
+    </button>
   </div>
 </template>
 
 
 
 <style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  width: 100%;
+  margin-top: 4%;
+}
 
+.card {
+  background-color: rgba(0, 0, 0, 0.121);
+  padding: 2% 2% 2% 2%;
+  
+}
 
+.btnCont {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.btn{
+  margin: 2% 2% 2% 2%;
+}
+.card h1{
+ color: white;
+}
+.card h2{
+  color: white;
+}
+.card p{
+  color: white;
+}
+.card h4{
+  color: white;
+}
 
+.btn {
+  display: inline-block;
+  padding: 10px 20px;
+  font-size: 16px;
+  text-align: center;
+  text-decoration: none;
+  cursor: pointer;
+  border-radius: 5px;
+  background-color: #774caf;
+  color: white;
+  border: none;
+  transition: background-color 0.3s;
+}
+
+.btn:hover {
+  background-color: #45a049;
+}
+
+.btn:active {
+  background-color: #3e8e41;
+}
 </style>
