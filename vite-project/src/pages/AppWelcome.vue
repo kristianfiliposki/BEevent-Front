260e79da5a1c2e964a1852d72e7da8f14b1030fb
ReDMallard <marco@titori.it>
2024-03-09 06:31:52 +0100

    aggiunge app welcome

---
+++ vite-project/src/pages/AppWelcome.vue
@@ -0,0 +1,31 @@
+<script>
+import { store } from '../store';
+import Appcard from '../components/Appcard.vue';
+import AppPresentation from '../components/AppPresentation.vue';
+export default {
+    name: "AppWelcome",
+    components: {
+        Appcard,
+        AppPresentation,
+    },
+    data() {
+        return {
+            store,
+        }
+    },
+    methods: {
+        selettore() {
+            let selector = this.$ref.selettore;
+            selector
+        }
+    },
+}
+</script>
+
+<template>
+    <AppPresentation />
+
+    <Appcard />
+    <router-view></router-view>
+</template>
+<style scoped></style>
